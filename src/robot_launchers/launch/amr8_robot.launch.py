28c2c77a79f990150e297929e9c7e4cd00ba4c35
ori-cogniteam <ori@cogniteam.com>
2024-12-01 10:46:03 +0200

    Running kobuki using docker

---
+++ src/robot_launchers/launch/amr8_robot.launch.py
@@ -0,0 +1,92 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
+from ament_index_python.packages import get_package_share_directory
+import os
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    # Get package directories
+    realsense_dir = get_package_share_directory('realsense2_camera')
+    sllidar_dir = get_package_share_directory('sllidar_ros2')
+    kobuki_dir = get_package_share_directory('kobuki')
+    slam_toolbox_dir = get_package_share_directory('slam_toolbox')
+    nav2_bringup_dir = get_package_share_directory('nav2_bringup')
+
+    # Create launch description objects
+    realsense_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(realsense_dir, 'launch', 'rs_launch.py')
+        )
+    )
+
+    sllidar_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(sllidar_dir, 'launch', 'sllidar_a2m8_launch.py')
+        ),
+        launch_arguments={
+            'scan_mode': 'Express',
+            'serial_port': '/dev/rplidar'
+        }.items()
+    )
+
+    kobuki_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(kobuki_dir, 'launch', 'kobuki.launch.py')
+        )
+    )
+
+    # Include SLAM Toolbox and Navigation
+    slam_toolbox_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(slam_toolbox_dir, 'launch', 'online_async_launch.py')
+        )
+    )
+
+    nav2_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(nav2_bringup_dir, 'launch', 'navigation_launch.py')
+        )
+    )
+
+    # Add new transformations
+    tf_footprint2base_cmd = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        output='screen',
+        arguments=['0.0', '0.0', '0.0', '0.0', '0.0', '0.0', 'base_link', 'base_footprint']
+    )
+
+    fake_bumper_cmd = Node(
+        package='kobuki',
+        executable='fake_bumer_node',
+        output='screen'
+    )
+
+    from_base_to_lidar_cmd = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        output='screen',
+        arguments=['0.0', '0.0', '0.3', '0.0', '0.0', '0.0', 'base_link', 'laser']
+    )
+
+    from_base_to_camera_cmd = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        output='screen',
+        arguments=['0.15', '0.0', '0.17', '0.0', '0.0', '0.0', 'base_link', 'camera_link']
+    )
+
+    return LaunchDescription([
+        realsense_launch,
+        sllidar_launch,
+        kobuki_launch,
+        slam_toolbox_launch,
+        nav2_launch,
+        tf_footprint2base_cmd,
+        fake_bumper_cmd,
+        from_base_to_lidar_cmd,
+        from_base_to_camera_cmd,
+    ]) 
